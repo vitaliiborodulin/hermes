06db5b451a4516d512d1855fb7091edb849959ac
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2025-04-11 13:12:53 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -52,6 +52,75 @@ $(function() {
     // 	}
     
     // });
+    // goods slider
+    $('.goods__slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        // infinite: true,
+        // speed: 500,
+        // arrows: false,
+        nextArrow: $('.goods .slider__arrow-next'),
+        prevArrow: $('.goods .slider__arrow-prev'),
+        // cssEase: 'linear',
+        // dots: true,
+        // fade: true,
+        // autoplay: false,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 834,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    });
+    
+    // article slider
+    $('.article__slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        // infinite: true,
+        // speed: 500,
+        // arrows: false,
+        nextArrow: $('.articles-sl .slider__arrow-next'),
+        prevArrow: $('.articles-sl .slider__arrow-prev'),
+        // cssEase: 'linear',
+        // dots: true,
+        // fade: true,
+        // autoplay: false,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 834,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    });
     $('.popup-link').fancybox({
         // thumbs : { autoStart:true },
         toolbar: "auto",
