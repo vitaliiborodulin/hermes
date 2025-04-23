5d715868dc0b631cc2dd32636b1b79a6d270859c
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2025-04-23 12:01:28 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -221,6 +221,10 @@ $(function() {
             preload : false
         }
     });
+    $('#myfile').change(function() {
+        if ($(this).val() != '') $(this).prev().text('Выбрано фото: ' + $(this)[0].files.length);
+        else $(this).prev().text('Выберите несколько фото');
+    });
 
     $('.catalog__select').niceSelect();
 
